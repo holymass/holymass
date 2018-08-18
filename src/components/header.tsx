8821c72149e583001a878213e1de831ac799c00e
Andrew Lyu <apemost@gmail.com>
2018-08-18 20:07:51 +0800

    Add drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,41 +12,91 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Hidden from '@material-ui/core/Hidden';
 import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import HomeIcon from '@material-ui/icons/Home';
+import InfoIcon from '@material-ui/icons/Info';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const styles = createStyles({
   flex: {
     flex: 1
   },
+  list: {
+    width: 300
+  }
 });
 
 export interface HeaderProps extends WithStyles<typeof styles> {
   brand: string;
 }
 
-class Header extends React.Component<HeaderProps, {}> {
+export interface HeaderState {
+  open: boolean;
+}
+
+class Header extends React.Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  }
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  }
+
   render() {
     const { classes, brand } = this.props;
-    const brandComponent = (
-      <Typography variant='title' align='center'>
-      {brand}
-      </Typography>
-    );
     return (
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton color='inherit' aria-label='Menu'>
-            <MenuIcon />
-          </IconButton>
-          <div className={classes.flex}>
-            <Hidden smDown implementation='css'>
-              {brandComponent}
+      <div>
+        <AppBar position='static'>
+          <Toolbar>
+            <Hidden smUp implementation='css'>
+              <IconButton color='inherit' aria-label='Menu' onClick={this.handleDrawerOpen}>
+                <MenuIcon />
+              </IconButton>
             </Hidden>
+            <div className={classes.flex}>
+              <Hidden smDown implementation='css'>
+      <Typography variant='title'>
+        {brand}
+      </Typography>
+              </Hidden>
+            </div>
+            <IconButton color='inherit' aria-label='Search'>
+              <SearchIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer anchor='left' open={this.state.open} onClose={this.handleDrawerClose}>
+          <div className={classes.list}>
+            <List component='nav'>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary={brand} />
+              </ListItem>
+            </List>
+            <Divider />
+            <List component='nav'>
+              <ListItem button>
+                <ListItemIcon>
+                  <InfoIcon />
+                </ListItemIcon>
+                <ListItemText primary='关于' />
+              </ListItem>
+            </List>
           </div>
-          <IconButton color='inherit' aria-label='Search'>
-            <SearchIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+        </Drawer>
+      </div>
     );
   }
 }
