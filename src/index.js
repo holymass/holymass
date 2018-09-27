468dbf9a9645dce3d35073e0f7f9e2cf45232c63
Andrew Lyu <apemost@gmail.com>
2018-09-28 00:07:34 +0800

    Use browser history

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import _ from 'lodash';
-import {createHashHistory} from 'history';
+import {createBrowserHistory} from 'history';
 import {Router} from 'react-router';
 import {config} from './data';
 import App from './components/app';
@@ -10,7 +10,7 @@ import App from './components/app';
 ReactGA.initialize(_.get(config, 'google.ga'));
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-const hist = createHashHistory();
+const hist = createBrowserHistory();
 
 ReactDOM.render((
   <Router history={hist}>
