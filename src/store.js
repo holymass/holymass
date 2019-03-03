b98b659b398ffa946155f47bc52f26f4d1cc69dd
Andrew Lyu <apemost@gmail.com>
2019-03-03 17:16:05 +0800

    Fix baseURL

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import axiosMiddleware from 'redux-axios-middleware';
 import rootReducer from './reducers';
 
 const axiosClient = axios.create({
-  baseURL: process.env.API_HOST || '/api',
+  baseURL: '/api',
   responseType: 'json',
 });
 
