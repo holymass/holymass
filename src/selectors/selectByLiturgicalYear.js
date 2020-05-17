bd3ddaa116efed83c414dc780179595fba4b2361
Andrew Lyu <apemost@gmail.com>
2020-05-17 14:26:15 +0800

    Ignore case while searching for masses
    Closes #6

--- src/selectors/selectByLiturgicalYear.js
+++ src/selectors/selectByLiturgicalYear.js
@@ -4,12 +4,13 @@ import _values from 'lodash/values';
 export default (data, liturgicalYear, filter) => {
   const filteredData = filter
     ? data[liturgicalYear].filter((item) => {
+        const f = filter.toLowerCase();
         return (
-          item.pinyin1.indexOf(filter) > -1 ||
-          item.pinyin2.indexOf(filter) > -1 ||
-          item.pinyin3.indexOf(filter) > -1 ||
-          item.pinyin4.indexOf(filter) > -1 ||
-          item.name.indexOf(filter) > -1
+          item.pinyin1.indexOf(f) > -1 ||
+          item.pinyin2.indexOf(f) > -1 ||
+          item.pinyin3.indexOf(f) > -1 ||
+          item.pinyin4.indexOf(f) > -1 ||
+          item.name.indexOf(f) > -1
         );
       })
     : data[liturgicalYear];
