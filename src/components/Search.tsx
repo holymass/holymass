620a16c86fcf352fea68e07229b6b0532a0e5f27
Andrew Lyu <apemost@gmail.com>
2022-05-21 22:16:38 +0800

    HOLYMASS-2 Add empty state

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import { useTranslation } from 'next-i18next';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Divider from '@mui/material/Divider';
 import InputAdornment from '@mui/material/InputAdornment';
 import InputBase from '@mui/material/InputBase';
-import ListItem from '@mui/material/ListItem';
 import SearchIcon from '@mui/icons-material/Search';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { green, grey } from '@mui/material/colors';
 import { alpha, styled, useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import FindInPageIcon from '@mui/icons-material/FindInPage';
 
 import MassCard from '../features/mass/MassCard';
 import ListMassesUseCase from '../features/mass/usecases/ListMassesUseCase';
@@ -58,9 +62,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export interface SearchProps {}
-
-export default function Search(props: SearchProps) {
+export default function Search() {
   const { t } = useTranslation('common');
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState<Mass | null>(null);
@@ -102,6 +104,7 @@ export default function Search(props: SearchProps) {
             renderInput={(params) => (
               <TextField
                 {...params}
+                autoFocus
                 variant="standard"
                 placeholder={t('Search')}
                 InputProps={{
@@ -147,7 +150,16 @@ export default function Search(props: SearchProps) {
           }}
         >
           {value && <MassCard model={value} />}
+          {!value && (
+            <Stack mt={4} mb={4} alignItems="center">
+              <FindInPageIcon sx={{ fontSize: 100, color: grey[300] }} />
+              <Typography variant="body2">{t('No results')}</Typography>
+            </Stack>
+          )}
         </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>{t('Close')}</Button>
+        </DialogActions>
       </Dialog>
     </SearchBox>
   );
