eb2558b55af8dd0401677d0517ac237a5b5e6b99
Andrew Lyu <apemost@gmail.com>
2022-05-22 15:42:50 +0800

    Optimize UpcomingMassGrid and MassTabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,15 @@
 import * as React from 'react';
 import type { NextPage, NextPageContext } from 'next';
-import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
 
 import Main from '../src/components/Main';
 import MassTabs from '../src/features/mass/MassTabs';
 import UpcomingMassGrid from '../src/features/mass/UpcomingMassGrid';
 
 const Home: NextPage = () => {
-  const { t } = useTranslation('mass');
   return (
     <Main>
-      <Typography variant="h5" mt={3}>
-        {t('Upcoming Masses')}
-      </Typography>
       <UpcomingMassGrid />
-      <Divider />
-      <Typography variant="h5" mt={3}>
-        {t('All Masses')}
-      </Typography>
       <MassTabs />
     </Main>
   );
